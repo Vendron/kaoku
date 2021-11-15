aec1e86b8e881c6a4cccb80b366e676bfe99b0a5
Vendron <gjc@clews.biz>
2021-11-14 18:49:43 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ client.once('ready', () => {
 });
 
 client.on("messageCreate", (message) => {
+    console.log(message.user.tag, message.user.bot, message.content)
     if (message.user.bot) return;
   
     if (message.content.toLowerCase().startsWith("!ping")) return message.channel.send({ content: "Pong!" });
   })
-
 client.login(config.token);
